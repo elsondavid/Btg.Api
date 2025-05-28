2c6d73bf7beb3c59d201710a2a86c61c48269fa0
ELSON DAVID FONSECA DA COSTA <elson@liberaai.com>
2025-05-28 14:43:52 -0300

    ok

--- Btg.Persistence/DbContextFactory.cs
+++ Btg.Persistence/DbContextFactory.cs
@@ -7,7 +7,7 @@ public class DbContextFactory : IDesignTimeDbContextFactory<DbContextBase>
     public DbContextBase CreateDbContext(string[] args)
     {
         var optionsBuilder = new DbContextOptionsBuilder<DbContextBase>();
-        optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=BtgDb;User Id=liberaaisa;Password=TurnKey#1;TrustServerCertificate=True;");
+        optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=BtgDb;User Id=xx;Password=xx;TrustServerCertificate=True;");
         return new DbContextBase(optionsBuilder.Options);
     }
 }
